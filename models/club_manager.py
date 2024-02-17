5f927c54b43d30dd6b8d34cee8adce3a04fe7471
Dwayne Pink <dwaynepink@gmail.com>
2024-02-16 17:21:25 -0800

    Added doc strings for notes and removed unused ccode.

--- models/club_manager.py
+++ models/club_manager.py
@@ -5,6 +5,10 @@ from .club import ChessClub
 
 
 class ClubManager:
+    """
+    Designed to manage chess clubs. iterates over all files in the specified folder,
+    creates a ChessClub object for each file and stores them in a JSON file.
+    """
     def __init__(self, data_folder="data/clubs"):
         datadir = Path(data_folder)
         self.data_folder = datadir
